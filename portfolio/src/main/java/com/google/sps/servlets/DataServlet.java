e2856a3b83f23e13ff1618269349b6274e293e95
Isabel Sangimino <isabelsang18@gmail.com>
2020-06-03 17:43:38 +0000

    handle edge case of empty comment

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -39,7 +39,9 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String message = request.getParameter("message");
-    comments.add(message);
+    if(!message.equals("")){
+        comments.add(message);
+    }
     response.sendRedirect("/index.html");
   }
 }
