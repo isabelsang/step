900ced12d17a206282a9f97e7f44823c57d14e71
Isabel Sangimino <isabelsang18@gmail.com>
2020-06-05 17:44:49 +0000

    update comment class to include mood

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -21,11 +21,13 @@ public final class Comment {
   private final String name;
   private final String message;
   private final long timestamp;
+  private final String mood;
 
   public Comment(long id, String name, String message, long timestamp) {
     this.id = id;
     this.name = name;
     this.message = message;
     this.timestamp = timestamp;
+    this.mood = mood;
   }
 }
