f9954a99ef67cc8a684d9c5363505d12a26de535
Isabel Sangimino <isabelsang18@gmail.com>
2020-06-11 18:59:12 +0000

    constant name conventions

--- portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
@@ -29,9 +29,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login-status")
 public class LoginStatusServlet extends HttpServlet {
   
-  private static final Gson gson = new Gson();
-  private static final String urlToRedirectToAfterUserLogsOut = "/";
-  private static final String urlToRedirectToAfterUserLogsIn = "/";
+  private static final Gson GSON = new Gson();
+  private static final String URL_REDIRECT_AFTER_LOG_OUT = "/";
+  private static final String URL_REDIRECT_AFTER_LOG_IN = "/";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -43,13 +43,13 @@ public class LoginStatusServlet extends HttpServlet {
       
       if (isUserLoggedIn) {
         loginStatus.put("email", userService.getCurrentUser().getEmail());
-        loginStatus.put("url", userService.createLogoutURL(urlToRedirectToAfterUserLogsOut));
+        loginStatus.put("url", userService.createLogoutURL(URL_REDIRECT_AFTER_LOG_OUT));
       } else {
         loginStatus.put("email", null);
-        loginStatus.put("url", userService.createLoginURL(urlToRedirectToAfterUserLogsIn));
+        loginStatus.put("url", userService.createLoginURL(URL_REDIRECT_AFTER_LOG_IN));
     }
 
-    String json = gson.toJson(loginStatus);
+    String json = GSON.toJson(loginStatus);
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
