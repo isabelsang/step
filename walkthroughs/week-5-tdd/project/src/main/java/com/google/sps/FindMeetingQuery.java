e1bec86e605b456b5b529c106f842bcc9a8974ba
Isabel Sangimino <isabelsang18@gmail.com>
2020-06-16 18:00:55 +0000

    passes tests of requested duration being too long and tests where there are no event conflicts

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -17,15 +17,39 @@ package com.google.sps;
 import java.util.Collection;
 import java.util.Arrays;
 import java.util.List;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
     Collection<TimeRange> times;
 
+    //Case in which the requested meeting has no attendies
     if(attendees.isEmpty()){
         return Arrays.asList(TimeRange.WHOLE_DAY);
     }
+
+    //Case in which the requested meeting has a duration that is too long/short
+    if(duration > TimeRange.WHOLE_DAY.duration() || duration < 0){
+        return Arrays.asList();
+    } 
+
+    //New collection of events that consist of events that the attendees are attending 
+    //and ignore ones that they are not
+    Collection<Event> relevantEvents = new HashSet<>();
+    for(String person : attendees){
+        for(Event event: events){
+            if(event.getAttendees().contains(person)){
+                relevantEvents.add(event);
+            }
+        }
+    }
+
+    if(relevantEvents.isEmpty()){
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+    
     return null;
   }
 }
