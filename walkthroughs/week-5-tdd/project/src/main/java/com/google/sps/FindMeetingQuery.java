8445a65937d597b4adb5a8f95cf214a8c55af203
Isabel Sangimino <isabelsang18@gmail.com>
2020-06-17 15:35:17 +0000

    algorithm in process, 2 more tests passed, working on other 5 remaining tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,15 +15,16 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     Collection<String> attendees = request.getAttendees();
     long duration = request.getDuration();
-    Collection<TimeRange> times;
 
     //Case in which the requested meeting has no attendies
     if(attendees.isEmpty()){
@@ -35,21 +36,71 @@ public final class FindMeetingQuery {
         return Arrays.asList();
     } 
 
-    //New collection of events that consist of events that the attendees are attending 
-    //and ignore ones that they are not
-    Collection<Event> relevantEvents = new HashSet<>();
+    //List of time ranges of other events that attendees are attending
+    List<TimeRange> timeRanges = new ArrayList<TimeRange>();
+    TimeRange currentRange;
     for(String person : attendees){
         for(Event event: events){
-            if(event.getAttendees().contains(person)){
-                relevantEvents.add(event);
+            currentRange = event.getWhen();
+            if(event.getAttendees().contains(person) && !timeRanges.contains(currentRange)){
+                timeRanges.add(currentRange);
             }
         }
     }
 
-    if(relevantEvents.isEmpty()){
+    //Case in which no events on calendar so no conflicts
+    if(timeRanges.isEmpty()){
         return Arrays.asList(TimeRange.WHOLE_DAY);
     }
-    
-    return null;
+
+    //Sort timeRanges by starting time 
+    Collections.sort(timeRanges, TimeRange.ORDER_BY_START);
+
+    Collection<TimeRange> availTimes = new ArrayList<TimeRange>(); //Time ranges that are available for the meeting request
+    int startOfAvail = TimeRange.START_OF_DAY; //The start of an available time range
+    int endOfAvail; 
+    int timeRangesCtr = 0; //Index of current position in array list "timeRanges"
+    TimeRange lastTimeRange = null; //Last time range that has been accounted for from timeRanges
+    TimeRange nextTimeRange = timeRanges.get(0);; //Next time range that needs to be accounted for
+
+    if(nextTimeRange.start() == TimeRange.START_OF_DAY){
+        //If earliest event starts at 00:00 then update startOfAvail 
+        //to be the end time of the first event
+        startOfAvail = nextTimeRange.end();
+
+        //First event is accounted for, so update lastTimeRange
+        lastTimeRange = nextTimeRange;
+        timeRangesCtr++;
+    } 
+
+    while(startOfAvail < TimeRange.END_OF_DAY && timeRangesCtr < timeRanges.size()){ 
+        nextTimeRange = timeRanges.get(timeRangesCtr);
+
+        if((null != lastTimeRange) && (lastTimeRange.overlaps(nextTimeRange))) {
+            startOfAvail = nextTimeRange.end();
+            timeRangesCtr++;
+        } else if ((null != lastTimeRange) && (lastTimeRange.contains(nextTimeRange))){
+            lastTimeRange = nextTimeRange;
+            timeRangesCtr++;
+        } else {
+            endOfAvail = nextTimeRange.start() - 1;
+            if((endOfAvail - startOfAvail) >= duration){
+                //Enough available time for the meeting so add it to the array list
+                availTimes.add(TimeRange.fromStartEnd(startOfAvail, endOfAvail, true));
+            }
+
+            lastTimeRange = nextTimeRange;
+            startOfAvail = nextTimeRange.end();
+            timeRangesCtr++;
+        }
+
+        //Check if there is enough time for requested meeting after last event
+        endOfAvail = TimeRange.END_OF_DAY;
+        if((startOfAvail < TimeRange.END_OF_DAY) && ((endOfAvail - startOfAvail) >= duration)){
+           availTimes.add(TimeRange.fromStartEnd(startOfAvail, endOfAvail, true)); 
+        }
+    }
+
+    return availTimes;
   }
 }
